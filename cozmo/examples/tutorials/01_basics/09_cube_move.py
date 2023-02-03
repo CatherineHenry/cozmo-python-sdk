598b50ffd840463d9ff5724e5575bc164ed86f6e
catherinehenry <catherinemshenry@gmail.com>
2023-02-03 10:12:41 -0700

    reorg directories so I can install as package locally

---
+++ cozmo/examples/tutorials/01_basics/09_cube_move.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2017 Anki, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License in the file LICENSE.txt or at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+'''Control Cozmo's Cube lights
+
+This script shows how you can control Cozmo's cube lights and set
+them to different colors - to red, green and blue in this case.
+'''
+
+import time
+
+import cozmo
+from cozmo.objects import LightCube1Id, LightCube2Id, LightCube3Id
+
+
+def cozmo_program(robot: cozmo.robot.Robot):
+    cube1 = robot.world.get_light_cube(LightCube1Id)  # looks like a paperclip
+    cube2 = robot.world.get_light_cube(LightCube2Id)  # looks like a lamp / heart
+    cube3 = robot.world.get_light_cube(LightCube3Id)  # looks like the letters 'ab' over 'T'
+
+    if cube1 is not None:
+        cube1.set_lights(cozmo.lights.red_light)
+    else:
+        cozmo.logger.warning("Cozmo is not connected to a LightCube1Id cube - check the battery.")
+
+    if cube2 is not None:
+        cube2.set_lights(cozmo.lights.green_light)
+    else:
+        cozmo.logger.warning("Cozmo is not connected to a LightCube2Id cube - check the battery.")
+
+    if cube3 is not None:
+        cube3.set_lights(cozmo.lights.blue_light)
+    else:
+        cozmo.logger.warning("Cozmo is not connected to a LightCube3Id cube - check the battery.")
+
+    # Keep the lights on for 10 seconds until the program exits
+    # time.sleep(10)
+
+    while True:
+        # print(f"Last moved time: {cube3.last_moved_time}")
+        # print(f"last observed time: {cube3.last_observed_time}")
+        # print(f"last tapped time: {cube3.last_tapped_time}")
+        acceleration = cube3.acceleration
+        # print(f"acceleration: {acceleration}")
+        if acceleration is not None:
+            x = acceleration.x
+            y = acceleration.y
+            # z = acceleration.z
+
+            vertical = "UP" if x < 0 else "DOWN"
+            horizontal = "LEFT" if y < 0 else "RIGHT"
+
+            print(f"{vertical} {horizontal}")
+
+        # time.sleep(1)
+
+
+cozmo.run_program(cozmo_program)
